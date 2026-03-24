667c063a796806be11c69f322f5384d01ea695c1
rachel490 <ccori12@naver.com>
2026-03-24 17:40:13 +0900

    chore: upgrade to React 18 and migrate

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { server } from './_tosslib/server/browser';
@@ -7,11 +7,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 server.start({ onUnhandledRequest: 'bypass' });
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
